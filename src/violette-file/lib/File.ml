d11106b2a905bbcb6c687de6f4070529dfa268e7
Qexat <43090614+qexat@users.noreply.github.com>
2025-09-02 11:27:41 +0200

    file: add utility functions

--- src/violette-file/lib/File.ml
+++ src/violette-file/lib/File.ml
@@ -14,3 +14,29 @@ let repr { path : string; contents : string } : Fmt.t =
     [ ("path", Repr.string path)
     ; ("contents", Repr.string contents)
     ]
+
+let lines (file : t) : string list =
+  String.split_on_char '\n' file.contents
+
+let lines_in_range
+      ~from:(start : int)
+      ~to_:(end_ : int)
+      (file : t)
+  : string list
+  =
+  List.filteri
+    (fun index _ -> start <= index + 1 && index + 1 <= end_)
+    (lines file)
+
+let enumerate_lines_in_range
+      ~from:(start : int)
+      ~to_:(end_ : int)
+      (file : t)
+  : (int * string) list
+  =
+  List.mapi
+    (fun index line -> (start + index + 1, line))
+    (lines_in_range ~from:start ~to_:end_ file)
+
+let line (number : int) (file : t) : string option =
+  List.nth_opt (lines file) number
