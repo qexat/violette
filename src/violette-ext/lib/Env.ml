88499ed9fb53dc61ae49480695b2570bb9c33576
Qexat <43090614+qexat@users.noreply.github.com>
2025-09-02 11:34:45 +0200

    replace former error system completely

--- src/violette-ext/lib/Env.ml
+++ src/violette-ext/lib/Env.ml
@@ -1,8 +1,4 @@
 type 'value t = (string * 'value) list
 
-let fetch ~(error : 'error) (name : string) (env : 'value t)
-  : ('value, 'error) result
-  =
-  match List.assoc_opt name env with
-  | None -> Error error
-  | Some value -> Ok value
+let fetch (name : string) (env : 'value t) : 'value option =
+  List.assoc_opt name env
